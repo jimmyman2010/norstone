95e9c4112f08ff689267a3bff157cb6590670801
Jimmy Man <tranduyminhman@gmail.com>
2015-04-11 11:30:57 -0700

    remove configurational file for local

--- _protected/common/config/main.php
+++ _protected/common/config/main.php
@@ -4,6 +4,21 @@ return [
     //'language' => 'en',
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=norstone',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
         'assetManager' => [
             'bundles' => [
                 // we will use bootstrap css from our theme
--- admin/index.php
+++ admin/index.php
@@ -9,9 +9,7 @@ require(__DIR__ . '/../_protected/backend/config/bootstrap.php');
 
 $config = yii\helpers\ArrayHelper::merge(
     require(__DIR__ . '/../_protected/common/config/main.php'),
-    require(__DIR__ . '/../_protected/common/config/main-local.php'),
-    require(__DIR__ . '/../_protected/backend/config/main.php'),
-    require(__DIR__ . '/../_protected/backend/config/main-local.php')
+    require(__DIR__ . '/../_protected/backend/config/main.php')
 );
 
 $application = new yii\web\Application($config);
