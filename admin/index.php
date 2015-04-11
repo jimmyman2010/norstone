4a8babed0fba590fd0b5ab2823700dd3c3d0c155
Jimmy Man <tranduyminhman@gmail.com>
2015-04-11 11:45:08 -0700

    add gitignore

--- admin/index.php
+++ admin/index.php
@@ -9,7 +9,9 @@ require(__DIR__ . '/../_protected/backend/config/bootstrap.php');
 
 $config = yii\helpers\ArrayHelper::merge(
     require(__DIR__ . '/../_protected/common/config/main.php'),
-    require(__DIR__ . '/../_protected/backend/config/main.php')
+    require(__DIR__ . '/../_protected/common/config/main-local.php'),
+    require(__DIR__ . '/../_protected/backend/config/main.php'),
+    require(__DIR__ . '/../_protected/backend/config/main-local.php')
 );
 
 $application = new yii\web\Application($config);
--- index.php
+++ index.php
@@ -9,7 +9,9 @@ require(__DIR__ . '/_protected/frontend/config/bootstrap.php');
 
 $config = yii\helpers\ArrayHelper::merge(
     require(__DIR__ . '/_protected/common/config/main.php'),
-    require(__DIR__ . '/_protected/frontend/config/main.php')
+    require(__DIR__ . '/_protected/common/config/main-local.php'),
+    require(__DIR__ . '/_protected/frontend/config/main.php'),
+    require(__DIR__ . '/_protected/frontend/config/main-local.php')
 );
 
 $application = new yii\web\Application($config);
