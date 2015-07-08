49a6579962977cbe9a81d8e640317ee127d4817c
jimmyman2010 <tranduyminhman@gmail.com>
2015-07-08 17:30:28 +0700

    changed to facebook comments & scroll up when click on tabs

--- themes/vitinhgiatot/assets/js/global.js
+++ themes/vitinhgiatot/assets/js/global.js
@@ -5,4 +5,10 @@ $(function(){
     $('.category-sorting .dropdownbox select').on('change', function() {
         window.location.href = $(this).val();
     });
+    $('li[role="presentation"] > a').on('click', function(){
+        var that = $(this);
+        $('html,body').animate({
+            scrollTop: that.offset().top - 60
+        }, 1000);
+    });
 });
